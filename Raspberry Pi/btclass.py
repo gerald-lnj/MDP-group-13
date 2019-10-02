cfd80df3f1b3672c542bfdbc7f3bb5bffe77ddc3
wongwanting <wong1089@e.ntu.edu.sg>
2019-10-02 12:07:10 +0800

    Update btclass.py

--- Raspberry Pi/btclass.py
+++ Raspberry Pi/btclass.py
@@ -47,12 +47,11 @@ class btconnection():
 
     def bt_send_msg(self, data):
         try:
-            #client_sock = self.client_sock
             client_sock.send(data)
         except IOError:
             print("Failed to send message to Android: {}".format(IOError))
 
-    def BT_checkStatus():
+    def bt_checkStatus():
         nexus_MAC_addr = '68:B3:5E:58:96:CB'
         stdoutdata = sp.check_output(["hcitool","con"])
         if nexus_MAC_addr in stdoutdata.split():

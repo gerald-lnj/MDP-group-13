a9eadf0d41f2eb44b16c96dd48744ed0e75e6304
wongwanting <wong1089@e.ntu.edu.sg>
2019-10-03 16:28:50 +0800

    Update main.py

--- Raspberry Pi/main.py
+++ Raspberry Pi/main.py
@@ -4,18 +4,20 @@ import Queue
 import time
 from btclass import *
 from arclass import *
+from tcpclass import *
 
 class Main(threading.Thread):
     def __init__(self):
         threading.Thread.__init__(self)
 
-        #self.algo_thread = algo()
         self.bt_thread = bt_connection()
         self.sr_thread = ard_connection()
+        self.pc_thread = tcp_connection()
         
         #initialise connections
         self.bt_thread.setup()
         self.sr_thread.setup()
+        self.pc_thread.setup()
         time.sleep(1)	# wait for 1 secs before starting
 
     #process to read from bluetooth
@@ -35,10 +37,11 @@ class Main(threading.Thread):
                 print("Sending message to Arduino...")
                 self.write_to_arduino(read_bt_msg[3:])
 
-            # Check header and send data to algo
-			# elif(read_bt_msg[0:2].lower() == 'al:'):	# send to algo
-			# 	self.writePC(read_bt_msg[3:])	# strip the header
-			# 	print "Value received from Bluetooth: %s" % read_bt_msg[1:]
+            # Check header and send to pc
+			elif(read_bt_msg[0:2].lower() == 'al:'):
+                print("Message Received from BT: {}".format(read_bt_msg))
+                print("Sending message to PC...")
+                self.write_to_pc(read_bt_msg[3:])
         
 			else:
 			    print ("Incorrect header received from BT: {}".format(read_bt_msg[0:2])) 
@@ -66,22 +69,55 @@ class Main(threading.Thread):
                 print("Sending message to Android...")
                 self.write_to_bluetooth(read_ard_msg[3:])
 
+            #Check header and send to pc
+            elif(read_bt_msg[0:2].lower() == 'al:'):
+                print("Message Received from Arduino: {}".format(read_ard_msg))
+                print("Sending message to PC...")
+                self.write_to_pc(read_ard_msg[3:])
+
     #process to write to arduino
     def write_to_arduino(self, msg_to_ard):
         self.sr_thread.ard_send_msg(msg_to_ard)
 
+    #process to read from pc
+    def read_from_pc(self):
+        while True:
+            
+            #Get message from pc
+            read_pc_msg = self.pc_thread.pc_listen_msg()
+
+            #Check header and send to android
+            if(read_pc_msg[0:2].lower() == 'an:'):
+                print("Message Received from PC: {}".format(read_pc_msg))
+                print("Sending message to Android...")
+                self.write_to_bluetooth(read_pc_msg[3:])
+
+            #Check header and send to arduino
+            elif(read_pc_msg[0:2].lower() == 'ar:'):
+                print("Message Received from PC: {}".format(read_pc_msg))
+                print("Sending message to Arduino...")
+                self.write_to_arduino(read_pc_msg[3:])
+
+            else:
+                print ("Incorrect header received from PC: {}".format(read_pc_msg[0:2])) 
+			    time.sleep(1)
+
+    #process to write to pc
+    def write_to_pc(self, msg_to_pc):
+        self.pc_thread.pc_send_msg()
+
     def initialize_threads(self):
         # Bluetooth (BT) read and write thread
 		read_bt_thread = threading.Thread(target = self.read_from_bluetooth, name = "bt_read_thread")
-		# print "created rt_bt"
 		write_bt_thread = threading.Thread(target = self.write_to_bluetooth, args = ("",), name = "bt_write_thread")
-		# print "created wt_bt"
 
 		# Serial (SR) read and write thread
 		read_ard_thread = threading.Thread(target = self.read_from_arduino, name = "sr_read_thread")
-		# print "created rt_sr"
 		write_ard_thread = threading.Thread(target = self.write_to_arduino, args = ("",), name = "sr_write_thread")
-		# print "created wt_sr"
+
+        #TCP (PC) read and write thread
+        read_pc_thread = threading.Thread(target = self.read_from_pc, name = "pc_read_thread")
+        write_pc_thread = threading.Thread(target = self.write_to_pc, args = ("",), name = "pc_write_thread")
 
         # Set threads as daemons
 		read_bt_thread.daemon = True
@@ -90,20 +126,26 @@ class Main(threading.Thread):
 		read_ard_thread.daemon = True
 		write_ard_thread.daemon = True
 
+        read_pc_thread.daemon = True
+        write_pc_thread.daemon = True
+
         # Start Threads
 		read_bt_thread.start()
 		write_bt_thread.start()
 
 		read_ard_thread.start()
 		write_ard_thread.start()
+
+        read_pc_thread.start()
+        write_pc_thread.start()
 	
 		print("All threads initialized succesfully")
 
 	def close_all_sockets(self):
         #close all sockets
-		#pc_thread.close_all_pc_sockets()
 		bt_thread.bt_disconnect()
 		sr_thread.ard_disconnect()
+        pc_thread.pc_disconnect()
 		print ("End threads")
 
 	def keep_main_alive(self):
@@ -113,7 +155,9 @@ class Main(threading.Thread):
 			time.sleep(0.5)
 
 if __name__ == "__main__":
-	mainThread = Main()
-	mainThread.initialize_threads()
-	mainThread.keep_main_alive()
-	mainThread.close_all_sockets()
+    try:
+	    mainThread = Main()
+	    mainThread.initialize_threads()
+	    mainThread.keep_main_alive()
+    except KeyboardInterrupt:
+	    mainThread.close_all_sockets()

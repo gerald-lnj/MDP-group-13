091f48316c886414a7c1213b2f359ed3714b7344
Gerald <gerald.lau.95@gmail.com>
2019-10-15 17:16:14 +0800

    update arduino comms (INCOMPLETE)

--- Raspberry Pi/arclass.py
+++ Raspberry Pi/arclass.py
@@ -34,6 +34,8 @@ class ard_connection():
 
     def ard_send_msg(self, data):
         try:
+            #TODO: ensure that arduino can also read a byte array
+            data_bytes = bytes(data, 'utf-8')
             self.s1.write(data)
         except IOError:
             print("Failed to send message to Arduino: {}".format(IOError))

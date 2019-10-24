a2a735d59d6fce43a2f2320d3652153ad4f00497
feliceeetan <felicetan3108@gmail.com>
2019-10-24 14:30:50 +0800

    Image Obstacle Location - Updated

--- Android/app/src/main/java/com/example/mdp_android/tabs/ImageCheckFragment.java
+++ Android/app/src/main/java/com/example/mdp_android/tabs/ImageCheckFragment.java
@@ -14,6 +14,7 @@ import android.widget.TextView;
 
 import com.example.mdp_android.Constants;
 import com.example.mdp_android.MainActivity;
+import com.example.mdp_android.Maze;
 import com.example.mdp_android.R;
 
 import java.lang.reflect.Array;
@@ -38,14 +39,12 @@ public class ImageCheckFragment extends Fragment implements MainActivity.Callbac
 
         TableLayout imageTable = (TableLayout) getView().findViewById(R.id.image_table);
 
-        while (imageTable.getChildCount() > 1)
-        {
+        while (imageTable.getChildCount() > 1) {
             TableRow row =  (TableRow)imageTable.getChildAt(1);
             imageTable.removeView(row);
         }
 
-        for (int i = 1; i < _imageDataArr.length; i += 4)
-        {
+        for (int i = 1; i < _imageDataArr.length; i += 4) {
             TableRow tbrow = new TableRow(getActivity());
 
             TextView t1v = new TextView(getActivity());
@@ -75,10 +74,8 @@ public class ImageCheckFragment extends Fragment implements MainActivity.Callbac
             imageTable.addView(tbrow);
         }
 
-        if (_imageDataArr.length > 4)
-        {
+        if (_imageDataArr.length > 4) {
             String[][] imgStringArr = new String[(_imageDataArr.length - 1) / 4][3];
-
             int j = 1;
             for (int i = 1; i < (_imageDataArr.length - 1) / 4 + 1; i++) {
                 imgStringArr[i - 1][0] = _imageDataArr[j];
@@ -102,54 +99,65 @@ public class ImageCheckFragment extends Fragment implements MainActivity.Callbac
     @Override
     public void update(int type, String key, String msg) {
         {
-            if (key != null)
-            {
+            if (key != null) {
                 key = key.trim();
             }
 
-            if (msg != null)
-            {
+            if (msg != null) {
                 msg = msg.trim();
             }
 
-            if (type == Constants.MESSAGE_READ && key.equals("IMAGE"))
-            {
-                String[] newImageEntry = msg.split("-");
-
-                if (_imageDataArr.length < 4)
-                {
-                    _imageDataArr = joinArray(_imageDataArr, newImageEntry);
-                }
-
-                if (_imageDataArr.length > 4)
-                {
-                    for(int j = 1; j < _imageDataArr.length ; j += 4)
-                    {
-                        if (newImageEntry[0].equals(_imageDataArr[j]))
-                        {
-                            _imageDataArr[j] = newImageEntry[0];
-                            _imageDataArr[j+1] = newImageEntry[2];
-                            _imageDataArr[j+2] = newImageEntry[1];
-                            _imageDataArr[j+3] = newImageEntry[3];
-                            break;
-                        }
-
-                        if (j + 4 == _imageDataArr.length)
-                        {
-                            _imageDataArr = joinArray(_imageDataArr, newImageEntry);
-                        }
-                    }
-                }
+            if (type == Constants.MESSAGE_READ && key.equals("IMAGE")) {
+                String[] tmp = msg.split("-");
+                String[] newImageEntry = MapFragment.getMaze().convertImgCoord(tmp).clone();
+                processNewImageEntry(newImageEntry);
                 onStart();
             }
-        }
-  }
 
-    static <T> T[] joinArray(T[]... arrays)
-    {
+            else if(type == Constants.MESSAGE_READ && key.equals("RESIMG")) {
+                String[] unresolvedImageDataArr = MapFragment.getMaze().resolveMisplacedImages();
+                if (unresolvedImageDataArr.length > 4) {
+                    String[][] imgStringArr = new String[(unresolvedImageDataArr.length - 1) / 4][4];
+                    int j = 1;
+                    for (int i = 0; i < (unresolvedImageDataArr.length - 1) / 4; i++) {
+                        imgStringArr[i][0] = unresolvedImageDataArr[j];
+                        imgStringArr[i][1] = unresolvedImageDataArr[j + 1];
+                        imgStringArr[i][2] = unresolvedImageDataArr[j + 2];
+                        imgStringArr[i][3] = unresolvedImageDataArr[j + 3];
+
+                        processNewImageEntry(MapFragment.getMaze().convertImgCoord(imgStringArr[i]).clone());
+
+                        j += 4;
+                    }
+                    onStart();
+                }
+            }
+        }
+    }
+
+    public void processNewImageEntry(String[] newImageEntry){
+        if (_imageDataArr.length < 4){
+            _imageDataArr = joinArray(_imageDataArr, newImageEntry);
+        }
+        if (_imageDataArr.length > 4){
+            for(int j = 1; j < _imageDataArr.length ; j += 4){
+                if (newImageEntry[0].equals(_imageDataArr[j])){
+                    _imageDataArr[j] = newImageEntry[0];
+                    _imageDataArr[j+1] = newImageEntry[2];
+                    _imageDataArr[j+2] = newImageEntry[1];
+                    _imageDataArr[j+3] = newImageEntry[3];
+                    break;
+                }
+                if (j + 4 == _imageDataArr.length) {
+                    _imageDataArr = joinArray(_imageDataArr, newImageEntry);
+                }
+            }
+        }
+    }
+
+    public static <T> T[] joinArray(T[]... arrays) {
         int length = 0;
-        for (T[] array : arrays)
-        {
+        for (T[] array : arrays) {
             length += array.length;
         }
 
@@ -157,12 +165,12 @@ public class ImageCheckFragment extends Fragment implements MainActivity.Callbac
         final T[] result = (T[]) Array.newInstance(arrays[0].getClass().getComponentType(), length);
 
         int offset = 0;
-        for (T[] array : arrays)
-        {
+        for (T[] array : arrays) {
             System.arraycopy(array, 0, result, offset, array.length);
             offset += array.length;
         }
 
         return result;
     }
+
 }
